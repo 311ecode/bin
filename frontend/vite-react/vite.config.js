74c59fbe02fac4bb3454849a2904ef8f2deba0c6
Imre Toth <tothimre@gmail.com>
2024-08-19 22:50:22 +0200

    making all the project files reacheable from the vite.

--- frontend/vite-react/vite.config.js
+++ frontend/vite-react/vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
+
   build: {
     sourcemap: true,  // Enable source maps for production builds
   },
+
   server: {
     sourcemap: true,  // Enable source maps for development server
   },
+
+  resolve: {
+    alias: {
+      '@mainProject': path.resolve(__dirname, './mainProject'),
+    }
+  }
+
 });
+
+// import { someFunction } from '@mainProject/lib/someFile';
