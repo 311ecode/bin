174943c356fa777e28182b7ee5ea3b20b299be5f
Imre Toth <tothimre@gmail.com>
2024-09-04 03:21:10 +0200

    Enhanced debug logging in verser.sh script
    - Introduced `verserDebug` function to replace direct echo statements.
      This enhances debugging by providing a conditional debug output.
    - Updated `loadVerser` and `unloadVerser` functions to use `verserDebug`
      for all internal logging.
    - Removed verbose echo outputs and replaced with structured debug
      logging for clarity and maintenance ease.
    - Simplified the `unloadVerser` function with conditional
      checks before unsetting variables.
    - General cleanup and removal of unused commented code blocks to
      improve script readability and maintainability.
    
    This change greatly improves the ability to trace and debug operations
    within the `verser.sh` script, allowing for more controlled and meaningful
    logging outputs based on the debug condition.

--- verser.sh
+++ verser.sh
@@ -6,6 +6,11 @@ verserHookPostfix="verser"
 
 createGitHookVariables verser "addSymlinksToGitExclude" "removeSymlinksFromGitExclude"
 
+verserDebug(){
+  createConditionalFunction  "echo" "DEBUG_VERSER" "debuggerVerser"  
+  debuggerVerser "$@"  
+}
+
 loadVerser() {
     GIT_PROJECT_HOOK_NAME="$verserHookPostfix"
     bashDir="$verserDir/bash"
@@ -14,7 +19,7 @@ loadVerser() {
     source $RC_LOADER
     if [ -z "$VERSER_LOAD_EXECUTED" ]; then
         loadAllRcFiles addPath development
-        echo "Current directory is within $verserDir"
+        verserDebug "Current directory is within $verserDir"
         createStateEngine $verserDir  "" "Verser"
         addStateVerser fuu faa
         # Add your commands here for when the condition is true
@@ -38,17 +43,20 @@ loadVerser() {
         linkToFrontendFromVerser configurationProcessor
         linkToFrontendFromVerser lib
         # after
-        echo "Verser functions loaded."
+        verserDebug "Verser functions loaded."
         # afterStateLoaded aaa
     fi
 }
 
-unloadVerser(){
-    echo "Unloading Verser functions."
-    unset GIT_PROJECT_HOOK_NAME
-    echo "GIT_PROJECT_HOOK_NAME:" $GIT_PROJECT_HOOK_NAME
+unloadVerser() {
+    verserDebug "Unloading Verser functions."
+    if [ "$GIT_PROJECT_HOOK_NAME" = "$verserHookPostfix" ]; then
+        unset GIT_PROJECT_HOOK_NAME
+        verserDebug "GIT_PROJECT_HOOK_NAME unset."
+    else
+        verserDebug "GIT_PROJECT_HOOK_NAME not unset: $GIT_PROJECT_HOOK_NAME"
+    fi
 }
-
 
 loadFilesInDirectory development "setupDirectoryHook, bindCtrlCombo";
 loadFilesInDirectory core "initRc, addPath"
@@ -62,55 +70,3 @@ initRc "RC_LOADER=\${RC_LOADER} source /home/imre/bin/verser.sh"
 initRc "everser() { e $verserDir; };  bindCtrlCombo x v everser;"
 initRc "verserJzf() { cd $verserDir; jzf; }; bindCtrlCombo x x v verserJzf;"
 # initRc "#********* Verser functions end *********#"
-
-
-
-# createSymlinkWrapper() {
-#     local source_dir="$1"
-#     shift  # Remove the first argument (source_dir) from the argument list
-
-#     local target_dir=$(mktemp -d)
-#     local random_suffix=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-#     local inner_function_name="inner_symlink_func_${random_suffix}"
-#     local outer_function_name="createSymlinkFunction_${random_suffix}"
-
-#     # Create the inner function using createSymlinkFunction
-#     createSymlink "$source_dir" "$target_dir" "$inner_function_name"
-
-#     # Create the outer wrapper function
-#     eval "
-#     $outer_function_name() {
-#         # Call the inner function with all received arguments
-#         $inner_function_name \"\$@\"
-
-#         # Print the target directory path to stdout
-#         echo \"$target_dir\"
-#     }
-#     "
-
-#     # Immediately use the created outer function with the remaining arguments
-#     local result=$($outer_function_name "$@")
-
-#     # Print the result (target directory path)
-#     echo "Symlinks created in: $result"
-
-#     if [ -n "${pathOnly+x}" ]; then
-#         echo "${target_dir}"
-#     else
-#         if command -v code &> /dev/null; then
-#             code "${target_dir}"
-#             echo "Opened Visual Studio Code in ${target_dir}"
-#         else
-#             echo "Visual Studio Code (command 'code') not found. Please open ${target_dir} manually."
-#         fi
-#     fi
-# }
-
-# function createSymlinkWrapperTest() {
-#     cls
-#     path=$(pathOnly=t createSymlinkWrapper ./ /home/imre/bin/verser.sh /home/imre/bin/frontend/vite-react/index.html /home/imre/bin/frontend/vite-react/| tail -n 1)
-#     echo  $path
-#     zed $path
-# }
-#
-# 1824  2024-08-28 04:41:29 create_symlink_wrapper . react.txt  configurationProcessor/apiRoutes/
